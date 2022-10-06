cd78294b49e5029e029d5ffba58341c14869cc11
Sergio Oseko <osk.sergio@gmail.com>
2022-10-05 22:23:21 -0300

    feat: implemented the associations in the models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ApplicationRecord
+  has_many :event_prizes
+  has_many :prizes, :through => :event_prizes
+
+  has_many :event_users
+  has_many :users, :through => :event_users
 end
--- app/models/prize.rb
+++ app/models/prize.rb
@@ -1,2 +1,4 @@
 class Prize < ApplicationRecord
+  has_many :event_prizes
+  has_many :events, :through => :event_prizes
 end
