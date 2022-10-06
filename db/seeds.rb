ad09a5e5f6289671fcae54ddedc40fb8dc52942e
Sergio Oseko <osk.sergio@gmail.com>
2022-10-06 06:53:28 -0300

    feat: implemented the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.create([
+               { description: 'Semana do Desenvolvedor 40+' },
+               { description: 'Palestra Motivacional I ' },
+               { description: 'Palestra: Empreendedor de Sucesso'} ])
+
+Prize.create([
+               { description: 'Livro: Clean Code' },
+               { description: 'Fone de ouvido bluetooth' },
+               { description: 'Curso: Empreendedor 10'},
+               { description: 'Agenda 2023'},
+               { description: 'Camiseta do Evento' } ])
+
+User.create ([
+               { name: 'Jhonn Travolta Jr.', age: 32, occupation: 'Advogado' },
+               { name: 'Mary Ann Cebajos', age: 26, occupation: 'Contador' },
+               { name: 'Júlio Prates Neto', age: 17, occupation: 'Estudante' },
+               { name: 'Carmen Miranda Martin', age: 48, occupation: 'Professora' } ])
+
+EventUser.create([
+               { event_id: 1, user_id: 2 }, { event_id: 1, user_id: 3 }, { event_id: 1, user_id: 4 },
+               { event_id: 2, user_id: 1 }, { event_id: 2, user_id: 3 }, { event_id: 2, user_id: 4 },
+               { event_id: 3, user_id: 1 }, { event_id: 3, user_id: 2 }, { event_id: 3, user_id: 3 }, { event_id: 3, user_id: 4 } ])
+
+EventPrize.create([
+                    { event_id: 1, prize_id: 2, description: 'Primeiro Prẽmio: fone', event_user_id: 2 },
+                    { event_id: 1, prize_id: 1, description: 'Segundo Prẽmio: livro', event_user_id: 3 },
+                    { event_id: 1, prize_id: 5, description: 'Terceiro Prẽmio: camiseta', event_user_id: 1 },
+                    { event_id: 2, prize_id: 2, description: 'Primeiro Prẽmio: fone', event_user_id: 4 },
+                    { event_id: 2, prize_id: 4, description: 'Segundo Prẽmio: agenda', event_user_id: 6 },
+                    { event_id: 2, prize_id: 5, description: 'Terceiro Prẽmio: camiseta', event_user_id: 5 },
+                    { event_id: 3, prize_id: 3, description: 'Primeiro Prẽmio: curso', event_user_id: 8 },
+                    { event_id: 3, prize_id: 2, description: 'Segundo Prẽmio: fone', event_user_id: 7 },
+                    { event_id: 3, prize_id: 5, description: 'Terceiro Prẽmio: camiseta', event_user_id: 10 } ])
+
+
+
+
